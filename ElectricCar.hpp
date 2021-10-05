634a2191b22848b1aeca4a185dc306a9b5994552
Gyrydymptum <gyrydymptum@gmail.com>
2021-10-05 12:30:30 +0200

    improvement of the interface

--- ElectricCar.hpp
+++ ElectricCar.hpp
@@ -1,16 +1,20 @@
+#pragma once
+#include "Car.hpp"
 #include "ElectricEngine.hpp"
 
-class ElectricCar
+
+class ElectricCar : virtual public Car
 {
 public:
     ElectricCar(ElectricEngine* engine);
     ~ElectricCar();
-    void turnLeft();
-    void turnRight();
-    void brake();
-    void accelerate(int speed);
-    void charge();
 
+    
+    void changeEngine(ElectricEngine*ee);
+    void refill() override;
+
+    private:
     ElectricEngine* engine_;
+    void charge();
 };
 
